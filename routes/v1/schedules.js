624879fd51d9ed52367dfc68db15716f8fb64825
Junaid Hassan <junaidte14@gmail.com>
2020-09-02 15:11:05 +0500

    adjustments

--- routes/v1/schedules.js
+++ routes/v1/schedules.js
@@ -16,55 +16,73 @@ router.post('/', authFunc.checkAdminAuthenticated, (req, res) => {
             notes: req.body.notes
         }); 
         newSchedule.save(function(err) {
-            if (err) return res.status(500).json({success: false, message: err.message});
-            mongoose.connection.close();
-            res.status(200).send(JSON.stringify({message: "Schedule is successfuly added!"}));
+            if (err){
+                return res.status(500).json({success: false, message: err.message});
+            }else{
+                mongoose.connection.close();
+                res.status(200).send(JSON.stringify({success: true, message: "Schedule is successfuly added!"}));
+            }
         });
     });
 });
 
 //read all Schedules
 router.get('/', (req, res) => {
-    Schedule.find({}).sort({"date":1}).exec(function(err, Schedules) {
-        if (err) return res.status(500).json({success: false, message: err.message});
-        mongoose.connection.close();
-        res.status(200).send(JSON.stringify({success: true, data: Schedules}));
+    Schedule.find({}).exec(function(err, Schedules) {
+        if (err){
+            return res.status(500).json({success: false, message: err.message});
+        }else{
+            mongoose.connection.close();
+            res.status(200).send(JSON.stringify({success: true, data: Schedules}));
+        }
     });
 });
 
 //read single Schedule by ID
 router.get('/:id', (req, res) => {
     Schedule.findById(req.params.id).exec(function(err, Schedule) {
-        if (err) return res.status(500).json({success: false, message: err.message});
-        mongoose.connection.close();
-        res.status(200).send(JSON.stringify({success: true, data: Schedule}));
+        if (err){
+            return res.status(500).json({success: false, message: err.message});
+        }else{
+            mongoose.connection.close();
+            res.status(200).send(JSON.stringify({success: true, data: Schedule}));
+        }
     });
 });
 
 //update single Schedule by ID
 router.post('/:id', authFunc.checkAdminAuthenticated, (req, res) => {
     Schedule.init().then(() => {
-        Schedule.findById(req.params.id).exec(function(err, Schedule) {
+        Schedule.findById(req.params.id).exec(function(err, response) {
             if (err) return res.status(500).json({success: false, message: err.message});
     
             if(req.body.name){
-                Schedule.name = req.body.name;
+                response.name = req.body.name;
             }
             if(req.body.ownerName){
-                Schedule.ownerName = req.body.ownerName;
+                response.ownerName = req.body.ownerName;
             }
             if(req.body.date){
-                Schedule.date = req.body.date;
+                response.date = req.body.date;
             }
             if(req.body.notes){
-                Schedule.notes = req.body.notes;
+                response.notes = req.body.notes;
             }
-            Schedule.dateUpdated = Date.now();
+            response.dateUpdated = Date.now();
          
-            Schedule.save(function(err) {
-                if (err) return res.status(500).json({success: false, message: err.message});
-                mongoose.connection.close();
-                res.status(200).send(JSON.stringify({success: true, message: "Schedule is successfully updated"}));
+            response.save(function(err) {
+                if (err){
+                    return res.status(500).json({success: false, message: err.message});
+                }else{
+                    Schedule.find({}).exec(function(err, schedules) {
+                        if (err){
+                            return res.status(500).json({success: false, message: err.message});
+                        }else{
+                            mongoose.connection.close();
+                            res.status(200).send(JSON.stringify({success: true, data: schedules}));
+                        }
+                    });
+                } 
             });
         });
     });
@@ -72,10 +90,19 @@ router.post('/:id', authFunc.checkAdminAuthenticated, (req, res) => {
 
 //delete single Schedule by ID
 router.delete('/:id', authFunc.checkAdminAuthenticated, (req, res) => {
-    Schedule.deleteOne({_id: req.params.id}).exec(function(err, Schedule) {
-        if (err) return res.status(500).json({success: false, message: err.message});
-        mongoose.connection.close();
-        res.status(200).send(JSON.stringify({success: true, message: "Schedule is successfully deleted"}));
+    Schedule.deleteOne({_id: req.params.id}).exec(function(err, response) {
+        if (err){
+            return res.status(500).json({success: false, message: err.message});
+        }else{
+            Schedule.find({}).exec(function(err, schedules) {
+                if (err){
+                    return res.status(500).json({success: false, message: err.message});
+                }else{
+                    mongoose.connection.close();
+                    res.status(200).send(JSON.stringify({success: true, data: schedules}));
+                }
+            });
+        }
     });
 });
 
